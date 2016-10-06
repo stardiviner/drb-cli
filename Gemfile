fd2bb6a8202b2d620d8f11737ac5bed41a017805
Steve Chan <chanshunli@gmail.com>
2016-10-06 20:12:35 +0800

    add drb server code

--- Gemfile
+++ Gemfile
@@ -1,4 +1,11 @@
-source 'https://rubygems.org'
+source 'https://gems.ruby-china.org'
 
 # Specify your gem's dependencies in drbcli.gemspec
+gem 'ruby_parser'
+gem 'method_source'
+group :development, :test do
+  gem 'pry'
+  gem 'rspec'
+end
+
 gemspec
--- lib/drbcli.rb
+++ lib/drbcli.rb
@@ -1,5 +1,31 @@
 require "drbcli/version"
+require 'ruby_parser'
+require 'method_source'
+require 'drb'
 
+$st_outputs = []
+class Object
+  include MethodSource::CodeHelpers
+  def stp (obj)
+    $st_outputs = $st_outputs + [obj]
+  end
+  def clear_st_outputs
+    $st_outputs = []
+  end
+  def drb_start drbenv
+    DRb.start_service 'druby://localhost:9000', drbenv
+    puts "Server running at #{DRb.uri}"
+    DRb.thread.join
+  end
+  # For ` alias rbsandbox=' RAILS_ENV=production rails c --sandbox '  `
+  def drb_start_pro drbenv
+    DRb.start_service 'druby://localhost:9018', drbenv
+    puts "Server running at #{DRb.uri}"
+    DRb.thread.join
+  end
+end
+
+# .. 
 module Drbcli
   # Your code goes here...
 end
